c4b40878f3ac736e265ccbb8ec9c128e5421c1a2
GuoXiaopeng <xiaopeng15@staff.sina.com.cn>
2019-12-19 09:31:14 +0800

    优化滚动

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,40 +9,9 @@ module.exports = {
 		lineNumbers: true,
 		anchor: {
 			permalink: false
-		},
-		extendMarkdown: md => {
-			// 使用更多的markdown-it插件
-			md.use(require("markdown-it-anchor"));
 		}
 	},
-	plugins: [
-		"@vuepress/active-header-links",
-		"@vuepress/back-to-top",
-		"@vuepress/last-updated",
-		"@vuepress/nprogress",
-		"vuepress-plugin-smooth-scroll",
-		[
-			("vuepress-plugin-medium-zoom",
-			{
-				selector: ".my-wrapper .my-img",
-				delay: 1000,
-				options: {
-					margin: 24,
-					background: "#BADA55",
-					scrollOffset: 0
-				}
-			})
-		],
-		[
-			"redirect",
-			{
-				// 提供多语言重定向功能
-				// 它会自动从 `/foo/bar/` 定向到 `/:locale/foo/bar/`，如果对应的页面存在
-				locales: true,
-				storage: true // 保存最后一次访问的结果到 `localStorage`，供下次重定向使用
-			}
-		]
-	],
+	plugins: [],
 	themeConfig: {
 		displayAllHeaders: true, // 默认值：false
 		smoothScroll: true,
@@ -56,13 +25,7 @@ module.exports = {
 				link: "https://github.com/lukeed/kleur"
 			}
 		],
-		sidebar: [
-			// {
-			//     title: "安装",
-			//     path: "/routes/install.html",
-			//     sidebarDepth: 2
-			// }
-		],
+		sidebar: [],
 		lastUpdated: "上次更新",
 		repo: ""
 	},
